da3437393ffee48c254fbc84f1a597a994487a8e
Marcos Oliveira <marcosluz312@hotmail.com>
2021-02-08 20:52:12 -0300

    Second day

---
+++ src/script.js
@@ -0,0 +1,16 @@
+const Modal = {
+    open(){
+        //Abrir modal
+        document
+            .querySelector('.modal-overlay')
+            .classList
+            .add('active')
+    },
+    close(){
+        //fechar modal
+        document
+        .querySelector('.modal-overlay')
+        .classList
+        .remove('active')
+    }    
+}
